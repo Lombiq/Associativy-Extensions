6e5912e3a8c83722d808f451d66d01f46dc7620a
Piedone <piedone@pyrocenter.hu>
2013-10-29 16:31:01 +0100

    The frontend engine for content parts is now configurable, contribution of Lombiq (http://lombiq.com/)

---
+++ Settings/AssociativyGraphLinksTypePartSettings.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Associativy.Frontends.EngineDiscovery;
+using Orchard.ContentManagement;
+using Orchard.ContentManagement.MetaData;
+using Orchard.ContentManagement.MetaData.Builders;
+using Orchard.ContentManagement.MetaData.Models;
+using Orchard.ContentManagement.ViewModels;
+using Orchard.Environment.Extensions;
+
+namespace Associativy.Extensions.Settings
+{
+    [OrchardFeature("Associativy.Extensions.ContentParts")]
+    public class AssociativyGraphLinksTypePartSettings
+    {
+        public string FrontendEngineName { get; set; }
+        public IEnumerable<IEngineDescriptor> AllFrontendEngines { get; set; }
+    }
+
+
+    [OrchardFeature("Associativy.Extensions.ContentParts")]
+    public class AssociativyGraphLinksTypePartSettingsHooks : ContentDefinitionEditorEventsBase
+    {
+        private readonly IEngineManager _engineManager;
+
+
+        public AssociativyGraphLinksTypePartSettingsHooks(IEngineManager engineManager)
+        {
+            _engineManager = engineManager;
+        }
+	
+			
+        public override IEnumerable<TemplateViewModel> TypePartEditor(ContentTypePartDefinition definition)
+        {
+            if (definition.PartDefinition.Name != "AssociativyGraphLinksPart")
+                yield break;
+
+            var settings = definition.Settings.GetModel<AssociativyGraphLinksTypePartSettings>();
+            settings.AllFrontendEngines = _engineManager.GetEngines();
+
+            yield return DefinitionTemplate(settings);
+        }
+
+        public override IEnumerable<TemplateViewModel> TypePartEditorUpdate(ContentTypePartDefinitionBuilder builder, IUpdateModel updateModel)
+        {
+            if (builder.Name != "AssociativyGraphLinksPart")
+                yield break;
+
+            var model = new AssociativyGraphLinksTypePartSettings();
+            updateModel.TryUpdateModel(model, "AssociativyGraphLinksTypePartSettings", null, null);
+            builder.WithSetting("AssociativyGraphLinksTypePartSettings.FrontendEngineName", model.FrontendEngineName);
+
+            yield return DefinitionTemplate(model);
+        }
+    }
+}
